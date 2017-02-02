ba39d27265106c4e4b36eb5e1158987ffd44930e
Anibal Santiago <anibal.santiago@expansion.com.mx>
2017-02-02 12:09:11 -0600

    add json test

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser')
 var app = express();
 
 app.use(bodyParser.raw({ type: 'application/*+xml' }));
- 
+ //app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.send('express online!!!');
@@ -21,6 +21,12 @@ app.get('/query', function (req, res) {
   res.send("User : "+user+ ", Age : "+age);
 });
 
+/*$ curl -d '{"MyKey":"My Value"}' -H "Content-Type: application/json" http://127.0.0.1:9293/test-page
+app.post('/test-page', function (req, res) {
+  console.log(request.body);      // your JSON
+   response.send(request.body);    // echo the result back
+});
+*/
 app.post('/test-page', function (req, res) {
   var body = req.body;
   console.log(body);
